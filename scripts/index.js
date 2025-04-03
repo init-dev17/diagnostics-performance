01809c340dd8a92cc3c289447da7318c529b31eb
init-dev17 <dev@init17.fr>
2025-04-03 10:48:59 +0200

    test url avec diagnostics-performance

--- scripts/index.js
+++ scripts/index.js
@@ -14,7 +14,7 @@ for(let site in presentation) {
   const newSlide = document.createElement("div");
   newSlide.classList.add("swiper-slide");
   newSlide.id = `slide${Number(site)+1}`;
-  newSlide.style.background = `url(/public/assets/img/${presentation[site].img}) fixed center/cover`;
+  newSlide.style.background = `url(/diagnostics-performance/public/assets/img/${presentation[site].img}) fixed center/cover`;
 // newSlide.style.background = `linear-gradient(rgba(0, 0, 0, 0.527),rgba(0, 0, 0, 0.5)), url(../public/assets/img/${presentation[site].img}) fixed center/cover`;
 
 
